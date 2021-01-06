bc94cffb977747f4ad0f7dd28eaa1238bdfeb50d
unknown <address>
2021-01-06 20:03:20 +0530

    platform removed

--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    platform = new Ground(150,305,300,170);
+   
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -61,5 +60,4 @@ function draw(){
 
     bird.display();
 
-    platform.display();
 }
